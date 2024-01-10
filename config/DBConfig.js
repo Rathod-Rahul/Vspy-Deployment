60ec5d9a53e0ad8fbfee7e2137896b236e574cac
Rathod-Rahul <rahulaa5047@gmail.com>
2024-01-10 18:24:17 +0530

    db password changed

--- config/DBConfig.js
+++ config/DBConfig.js
@@ -3,7 +3,7 @@ require("dotenv").config();
 const connection = mysql.createConnection({
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
+  password: "",
   database: process.env.DB_DATABASE,
 });
 
